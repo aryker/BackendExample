4936040b86d0e0f8cbe0297323d83f51ff2da8cd
Alex Ryker <acryker@gmail.com>
2017-12-14 10:24:18 -0500

    Fix bugs.

--- register.php
+++ register.php
@@ -7,15 +7,17 @@
 	$grade = $_POST["grade"];
 	
 	//connect to the SQL database
-	$con = mysqli_connect("integrity", "techhound", "team868!", "techhound");
+	$con = mysqli_connect("integrity", "techhound", "team868!", "techhound"); //This is BAD PRACTICE! Don't do this on a real website! Read the credentials from a file instead!
 	if(mysqli_connect_errno($con)) {
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
 	
 	//insert new student information into database	
-	$statement = "INSERT INTO members (firstname, lastname, sex, grade) VALUES (\"" . $firstname . "\", \"" . $lastname . "\", " . substr($sex, 1) . ", " . $grade . ")";
+	$statement = "INSERT INTO members (firstname, lastname, sex, grade) VALUES (\"" . $firstName . "\", \"" . $lastName . "\", \"" . substr($sex, 0, 1) . "\", " . $grade . ")";
 	
 	mysqli_query($con, $statement);
+	
+	echo "Congratulations! You have been registered successfully.";
 	
 	//close the SQL connection
 	mysqli_close($con);
